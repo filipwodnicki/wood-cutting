2698589efae4b9ccccd0f9a4a0aff4c3813da5ed
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-07-04 16:34:33 +0200

    Board tested fully

--- dev/test_board.py
+++ dev/test_board.py
@@ -4,11 +4,46 @@ from dev.woodcut import Board
 
 
 class TestBoard(TestCase):
-    def test_insert(self):
+
+    def test_insert_type(self):
 
         b = Board()
 
         self.assertRaises(Exception, b.insert, 'car')
 
-    # def test_remove(self):
-    #     self.fail()
+    def test_insert_size(self):
+
+        b = Board()
+
+        with self.assertRaises(Exception):
+            b.insert(1000)
+            b.insert(1000)
+            b.insert(51)
+
+        b2 = Board()
+
+        self.assertRaises(Exception, b2.insert, 2051)
+
+    def test_insert_space(self):
+        b = Board()
+
+        b.insert(100)
+
+        self.assertEqual(b.space_remaining, 1950)
+
+        b.insert(850)
+
+        self.assertEqual(b.space_remaining, 1100)
+
+    def test_remove(self):
+
+        b = Board()
+        b.insert(50)
+
+        self.assertRaises(Exception, b.remove, 100)
+        self.assertEquals(b.space_remaining, 2000)
+
+        b.remove(50)
+
+        self.assertRaises(Exception, b.remove, 50)
+        self.assertEquals(b.space_remaining, 2050)
