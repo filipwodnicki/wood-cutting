f3ecffced163f5128a5ee9e66ff79e8c19e4da5d
bananalytics <31481575+bananalytics@users.noreply.github.com>
2018-07-05 15:56:39 +0200

    if __name__ == "__main__"

--- dev/main.py
+++ dev/main.py
@@ -2,9 +2,10 @@ from dev.algorithms.greedy import greedy_algorithm
 
 pieces = [450, 444, 436, 430, 389, 389, 386, 375, 362, 362, 261, 261, 261]
 
-result = greedy_algorithm(pieces)
+if __name__ == "__main__":
+    result = greedy_algorithm(pieces)
 
-for board in result:
-    print ( board.contents )
+    for board in result:
+        print ( board.contents )
 
 
